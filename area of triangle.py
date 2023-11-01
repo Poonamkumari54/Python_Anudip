6f6e1ae1adb531d0e060c0064471b531b6837da0
Poonam kumari <90674698+Poonamkumari54@users.noreply.github.com>
2023-11-01 23:03:13 +0530

    Add files via upload

---
+++ area of triangle.py
@@ -0,0 +1,8 @@
+b = 10
+h = 10
+
+# Calculate the area of the triangle using the formula: (base * height) / 2.
+area = b * h / 2
+
+# Print the calculated area of the triangle.
+print("area = ", area)
---
+++ find largest.py
@@ -0,0 +1,4 @@
+num1=10
+num2=15
+largest=num1 if num1>num2 else num2
+print("largest number is num1")
