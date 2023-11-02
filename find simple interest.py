141b32667ae11ab5e4828c665ac6a8736a7bd257
Poonam kumari <90674698+Poonamkumari54@users.noreply.github.com>
2023-11-02 21:47:29 +0530

    Update find simple interest.py

--- find simple interest.py
+++ find simple interest.py
@@ -1,14 +1,13 @@
 # Principal amount
 principal = 200
-
 # Annual interest rate (in percentage)
 rate = 5
-
 # Time period (in years)
 time = 5
-
 # Calculate simple interest
 simple_interest = (principal * rate * time) / 100
-
 # Print the result
 print("Simple Interest: Rs.", simple_interest)
+
+#Output
+#Simple Interest: Rs. 50.0
