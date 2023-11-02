ddac883c8e65a88053fb87bbc9c0de788e4454ab
Poonam kumari <90674698+Poonamkumari54@users.noreply.github.com>
2023-11-02 21:49:37 +0530

    Update leap year.py

--- leap year.py
+++ leap year.py
@@ -6,3 +6,7 @@ if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print(year, "is a leap year")
 else:
     print(year, "is not a leap year")
+
+#Output
+#Enter a year: 2023
+#2023 is not a leap year
