4a28a02c1ec77a72d984d0571a917e9f398916c1
Poonam kumari <90674698+Poonamkumari54@users.noreply.github.com>
2023-11-02 21:43:25 +0530

    Update convert km to miles.py

--- convert km to miles.py
+++ convert km to miles.py
@@ -5,3 +5,7 @@ conv_fac=0.621371
 #calculate miles
 miles=kilometers*conv_fac
 print("After converting kilometers into miles",miles)
+
+#Output
+#Enter value in kilometers:3.5
+After converting kilometers into miles 2.1747985
